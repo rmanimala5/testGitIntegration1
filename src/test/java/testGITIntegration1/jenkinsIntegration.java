b9c45d92974270a0f751376ff33a1e3df2949cfd
mani <rmanimala5@gmail.com>
2025-02-24 15:02:21 +0530

    "pushing the code"

---
+++ src/test/java/testGITIntegration1/jenkinsIntegration.java
@@ -0,0 +1,27 @@
+package testGITIntegration1;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class jenkinsIntegration {
+
+	@Test
+	public void test() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.dezlearn.com/webtable-example/");
+
+		System.out.println("launched");
+		String title = driver.getTitle();
+		
+		Reporter.log(title);
+		
+	}
+	
+	
+}
